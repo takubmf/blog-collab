b1af5fd137561c31829953e1e8a7e640a224dbbc
kn7767 <112599359+kn7767@users.noreply.github.com>
2023-01-03 15:51:42 -0800

    to-do list in readme

--- README.md
+++ README.md
@@ -1,1 +1,61 @@
 # blog-collab
+
+## What we need: 
+Add dates to each post
+Make a "posting" page
+    - Create a blog post (character limit?)
+        - Title
+        - Description
+    - Upload an item to marketplace
+        - Insert photo
+        - Title
+        - Price
+        - Item Description
+    * Must have labeled input boxes
+Seller profile
+    - Section to add and view bio
+    - Profile pic? 
+    - Separate pages for blog and market items
+Database (please change descriptors if necessary!!)
+    - Needs a database 
+        - Seller (profile?)
+            - Bio
+        - Seller (blog)
+            - Title
+            - Description 
+        - Seller (marketplace)
+            - Title
+            - Price
+            - Details
+CSS
+    - Mobile compatibility
+    - Framework (not bootstrap!!)
+        - Materialize (Kaylee has experience using it)
+        - Skeleton (best for simple sits)
+        - 
+    - Login page
+        - to do after the rest of the site is stylized to match
+ENV???
+
+## Reasonings for certain files:
+main.handlebars: 
+    - formats home page
+blog.handlebars: 
+    - individual blog posts
+    - {#each xyz} on homepage.handlebars makes a post based on format of blog.handlebars
+marketplace.handlebars: 
+    - individual items to sell
+    - "" ^^^
+sellerhome.handlebars: 
+    - seller profile
+    - shows both blogs and items 
+
+
+## Resources:
+https://handlebarsjs.com/guide/expressions.html#helpers
+https://codepen.io/meta-codes/pen/OJwVEqG
+
+
+
+
+
--- server.js
+++ server.js
@@ -1,16 +1,47 @@
-const express = require('express');
-const exphbs = require('express-handlebars');
-const path = require('path');
-const hbs = exphbs.create({});
-
+const path = require("path");
+const express = require("express");
+const routes = require("./controllers");
+const session = require("express-session");
+const exphbs = require("express-handlebars");
+const helpers = require("./utils/helpers");
+const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+// Setting
 const app = express();
 const PORT = process.env.PORT || 3001;
-
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(require('./controllers/dish-routes'));
-
-app.listen(PORT, () => {
-    console.log('Server listening on: http://localhost:' + PORT);
-  });
+// Creating App Session
+const sess = {
+  secret: "secret",
+  // Configure Cookie
+  cookie: {
+    maxAge: 1800000, // 30 Mins
+    httpOnly: true,
+    secure: false,
+    sameSite: "strict",
+  },
+  resave: false,
+  saveUninitialized: true,
+  //  Session Store
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+// Using Sequelize Session
+app.use(session(sess));
+const hbs = exphbs.create({ helpers });
+// Handlebars
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+// Passing Data in Body
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, "public")));
+// Routing
+app.use(routes);
+// Listening
+sequelize.sync({force: false})
+    .then(() => {
+      app.listen(PORT, () => {
+        console.log(`Now listening on http://localhost:${PORT}`);
+      });
+    });
