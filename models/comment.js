be142367c4278e5f59ee4e925390e3344312aed5
jhlynn95 <109389786+jhlynn95@users.noreply.github.com>
2023-01-03 14:01:53 -0800

    added js models

---
+++ models/comment.js
@@ -0,0 +1,48 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Comment extends Model { }
+
+Comment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    comment: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "user",
+        key: "id"
+      }
+    },
+    post_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "post",
+        key: "id"
+      }
+    },
+    date_created: {
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+      defaultValue: DataTypes.NOW
+    }
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "comment",
+  }
+);
+
+module.exports = Comment;
--- models/index.js
+++ models/index.js
@@ -1,11 +1,37 @@
-const User = require('./User');
+const Comment = require("./comment");
+const Post = require("./post");
+const User = require("./user");
 
-// Gallery.hasMany(Painting, {
-//   foreignKey: 'gallery_id',
-// });
 
-// Painting.belongsTo(Gallery, {
-//   foreignKey: 'gallery_id',
-// });
+User.hasMany(Post, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE"
+});
 
-module.exports = { User };
+User.hasMany(Comment, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE"
+});
+
+Post.hasMany(Comment, {
+  foreignKey: "post_id",
+  onDelete: "CASCADE"
+});
+
+Post.belongsTo(User, {
+  foreignKey: "user_id"
+});
+
+Comment.belongsTo(User, {
+  foreignKey: "user_id"
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: "post_id"
+});
+
+module.exports = {
+  Comment,
+  Post,
+  User
+};
