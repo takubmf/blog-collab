8465ac15358d9ae2868317da73599c71097f7bb6
jhlynn95 <109389786+jhlynn95@users.noreply.github.com>
2023-01-05 20:30:52 -0800

    set datbase

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,18 +2,28 @@ const express = require('express');
 const router = express.Router();
 
 router.get("/", (req,res) => {
-    res.send({ data: "Here is your data"});
+    res.render("homepage",{ data: "Here is your data"});
 })
 
-router.post("/", (req, res)=> {
-    res.send({ data: "User created"});
+router.get("/blog/:id", (req, res)=> {
+
+// Bring in a model and make a qquery for some data
+// const blogData = await Blog.findByPk(req.params.id)
+// const blog = blogData.get({plain:true})
+
+
+
+    res.render("blog",{ 
+        // blog,
+        data: "User created"
+    });
 })
 
-router.put("/", (req, res)=> {
+router.get("/", (req, res)=> {
     res.send({ data: "User updated"});
 })
 
-router.delete("/", (req, res)=> {
+router.get("/", (req, res)=> {
     res.send({ data: "User deleted"});
 })
 
