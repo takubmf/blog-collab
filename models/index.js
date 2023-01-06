be8b9cadd09fd7f1b0ee3b3484a4305b8ae4e14a
jhlynn95 <109389786+jhlynn95@users.noreply.github.com>
2023-01-06 12:13:11 -0800

    some clean up

--- models/index.js
+++ models/index.js
@@ -1,11 +1,4 @@
 const User = require('./User');
 
-// Gallery.hasMany(Painting, {
-//   foreignKey: 'gallery_id',
-// });
-
-// Painting.belongsTo(Gallery, {
-//   foreignKey: 'gallery_id',
-// });
 
 module.exports = { User };
