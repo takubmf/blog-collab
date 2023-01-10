1bcd0286e4523964b72ce9143244f5a2d7b1cb90
jhlynn95 <109389786+jhlynn95@users.noreply.github.com>
2023-01-09 17:20:21 -0800

    message setup

--- db/schema.sql
+++ db/schema.sql
@@ -3,3 +3,15 @@ DROP DATABASE IF EXISTS illuna;
 CREATE DATABASE illuna;
 
 USE illuna;
+
+CREATE TABLE app_user
+(
+	id int NOT NULL AUTO_INCREMENT,
+	name varchar(50) NOT NULL,
+    email varchar (50) NOT NULL,
+    password varchar (50) NOT NULL,
+    age INT,
+    location varchar (50) NOT NULL,
+    avatar varchar (200),
+	PRIMARY KEY (id)
+);
---
+++ models/message.js
@@ -0,0 +1,9 @@
+module.exports = function (sequelize, DataTypes) {
+    var Chat = sequelize.define("Chat", {
+      // eslint-disable-next-line camelcase
+      chat_message: DataTypes.STRING,
+      username: DataTypes.STRING,
+      room: DataTypes.STRING,
+    });
+    return Chat;
+  };
